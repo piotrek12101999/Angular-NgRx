2ffc609aa25727bd735e0f385c47627f6642bdfd
Piotr <piter.gazownik@o2.pl>
2019-04-21 17:38:43 +0200

    Done

---
+++ src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+import * as TutorialActions from './../actions/tutorial.action';
+
+const initialState: Tutorial = {
+  name: 'Initial Tutorial',
+  url: 'http://www.google.pl'
+};
+
+export function reducer(
+  state: Tutorial[] = [initialState],
+  action: TutorialActions.Actions
+) {
+  switch (action.type) {
+    case TutorialActions.ADD_TUTORIAL:
+      return [...state, action.payload];
+    case TutorialActions.REMOVE_TUTORIAL:
+      state.splice(action.payload, 1);
+      return state;
+    default:
+      return state;
+  }
+}
